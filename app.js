892b3d2491f415d1e122d608bbc7bebd8230e7eb
@sandyGonzalez <sandygonzalez.612@gmail.com>
2019-08-01 20:41:54 -0500

    Agregando imgenes y modificando variable classifier

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var fs = require('fs');
 //set IBM cloud credentials in this section
 var visualRecognition = new VisualRecognitionV3({
   version: '2019-05-29',
-  iam_apikey: 'Remplace aqui'
+  iam_apikey: '_3r_dz5i58xNUS-Q0VFCTS2D_YsP96VMWIC3TcwNztcE'
 });
 
 // Set The Storage Engine
@@ -72,7 +72,7 @@ app.post('/upload', (req, res) => {
         });
       } else {
         var images_file = fs.createReadStream(`./public/uploads/${req.file.filename}`);
-        var classifier_ids = ["Tiposdetaco_2107577272"];
+        var classifier_ids = ["Tacooos_2063189403"];
         console.log("datos de la imagen -- " + images_file);
         //
           var params = {
